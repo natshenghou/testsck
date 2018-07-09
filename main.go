23521f9f247f9ec5b9a43f3dc8f3bffa6f6be921
nat.la <nat.la@baac.or.th>
2018-07-09 14:55:19 +0700

    [Edited]: แก้ไขไฟล์ main.go

--- main.go
+++ main.go
@@ -5,12 +5,52 @@ import (
 	"database/sql"
 	_"github.com/go-sql-driver/mysql"
 )
+
+type UserData struct{
+	Id int
+	CitizenId string
+	Firstname string
+	Lastname string
+	BirthYear string
+	FirstnameFather string
+	FirstnameMother string
+	LastnameFather string
+	LastnameMother string
+	SoldierId int
+	AddressId int
+
+}
 func main(){
-	_, err := sql.Open("mysql","root@tcp(127.0.0.1:3306)/testsck")
+	db, err := sql.Open("mysql","root:@tcp(127.0.0.1:3306)/test")
 
 	if err != nil{
 		fmt.Println("connect fail")
 	}
 
 	fmt.Println("connect sucsess")
+
+	defer db.Close()
+
+	results,_ :=db.Query("SELECT * FROM user")
+
+	for results.Next() {
+		var userData UserData
+		err := results.Scan(
+			&userData.Id, 
+			&userData.CitizenId,
+			&userData.Firstname, 
+			&userData.Lastname,
+			&userData.BirthYear, 
+			&userData.FirstnameFather, 
+			&userData.LastnameFather,
+			&userData.FirstnameMother,
+			&userData.LastnameMother,
+			&userData.SoldierId,
+			&userData.AddressId,
+		) 
+		if err != nil {
+			panic(err.Error())
+		}	
+		fmt.Println(userData)
+	}
 }
