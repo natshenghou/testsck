3c4e20129a0d10d3ccd278265e9f5f92021648aa
nat.la <nat.la@baac.or.th>
2018-07-09 14:16:12 +0700

    [Created]: main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"database/sql"
+	_"github.com/go-sql-driver/mysql"
+)
+func main(){
+	_, err := sql.Open("mysql","root@tcp(127.0.0.1:3306)/testsck")
+
+	if err != nil{
+		fmt.Println("connect fail")
+	}
+
+	fmt.Println("connect sucsess")
+}
